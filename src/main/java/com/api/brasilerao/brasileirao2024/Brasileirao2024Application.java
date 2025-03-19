bf643b146385fb4e00eb5c9e5335c9bf5aa239b8
Eduardo Pereira Filho <eduardopereirafilho6@gmail.com>
2025-03-19 12:13:09 -0300

    starting project

---
+++ src/main/java/com/api/brasilerao/brasileirao2024/Brasileirao2024Application.java
@@ -0,0 +1,13 @@
+package com.api.brasilerao.brasileirao2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Brasileirao2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Brasileirao2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/brasilerao/brasileirao2024/Brasileirao2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.brasilerao.brasileirao2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Brasileirao2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
